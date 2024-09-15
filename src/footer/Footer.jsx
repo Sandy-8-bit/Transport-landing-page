64335044db9172b5a8f12490da8471516c6cb139
Sandy-8-bit <santhosh.vp.aiml.2022@snsct.org>
2024-09-15 22:16:37 +0530

    Update Footer.jsx

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
-import './Footer.css';
+import React from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate from react-router-dom
+import "./Footer.css";
 import logo from "./Eintransport-png.png";
 import group from "./insta.svg";
 import group2 from "./twitter.svg";
@@ -12,98 +12,140 @@ const Footer = () => {
   const navigate = useNavigate();
 
   const scrollToSection = (sectionId) => {
-    navigate('/');  // Ensure we're on the home page
+    navigate("/"); // Ensure we're on the home page
     setTimeout(() => {
       const section = document.getElementById(sectionId);
       if (section) {
         const yOffset = -50; // 10px offset from the top
-        const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
-        window.scrollTo({ top: y, behavior: 'smooth' });
+        const y =
+          section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+        window.scrollTo({ top: y, behavior: "smooth" });
       }
     }, 100); // Delay ensures the page navigates to home before scrolling
   };
-  
 
   return (
     <div className="app-footer">
       <div className="app-footer-div1">
         <div className="one-footer">
-            <img src={logo} alt='logo'/>
+          <img src={logo} alt="logo" />
         </div>
         <div className="two-footer">
-            <p> Welcome to Eintransport! We are a trusted and reliable packing and moving service dedicated to making your relocation experience smooth and hassle-free.</p>
+          <p>
+            {" "}
+            Welcome to Eintransport! We are a trusted and reliable packing and
+            moving service dedicated to making your relocation experience smooth
+            and hassle-free.
+          </p>
         </div>
         <div className="three-footer">
-        <a href="https://www.instagram.com/eintransport?igsh=MWdnZ29sb3dhb2J5Yw==" target="_blank"> 
-  <img src={group} alt='img'/>
-</a>
+          <a
+            href="https://www.instagram.com/eintransport?igsh=MWdnZ29sb3dhb2J5Yw=="
+            target="_blank"
+          >
+            <img src={group} alt="img" />
+          </a>
 
-<a href="https://x.com/eintranspo92563?t=OKi5L5XsKMl_OAF2-s4Ftw&s=09" target="_blank"> 
-  <img src={group2} alt='img'/>
-</a>
-<a href="https://www.facebook.com/profile.php?id=61565638056769&mibextid=kFxxJD" target="_blank"> 
-  <img src={group3} alt='img'/>
-</a>
-
+          <a
+            href="https://x.com/eintranspo92563?t=OKi5L5XsKMl_OAF2-s4Ftw&s=09"
+            target="_blank"
+          >
+            <img src={group2} alt="img" />
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=61565764442291&mibextid=ZbWKwL"
+            target="_blank"
+          >
+            <img src={group3} alt="img" />
+          </a>
         </div>
         <div className="four-footer">
-            <p>Copyright @2024</p>
+          <p>Copyright @2024</p>
         </div>
       </div>
       <div className="app-footer-div2">
         <div className="heading-foot">
-            <h5>MAY WE HELP YOU?</h5>
+          <h5>MAY WE HELP YOU?</h5>
         </div>
         <div className="heading-foo-res">
-            <h5>Quick links</h5>
+          <h5>Quick links</h5>
         </div>
         <div className="link-footer">
-            <p onClick={() => scrollToSection('services')} style={{ cursor: 'pointer' }}>Services</p>
-            <p onClick={() => scrollToSection('about-us')} style={{ cursor: 'pointer' }}>About Us</p>
-            <p onClick={() => scrollToSection('contact-us')} style={{ cursor: 'pointer' }}>Contact Us</p>
-            <p>Shipping</p>
+          <p
+            onClick={() => scrollToSection("services")}
+            style={{ cursor: "pointer" }}
+          >
+            Services
+          </p>
+          <p
+            onClick={() => scrollToSection("about-us")}
+            style={{ cursor: "pointer" }}
+          >
+            About Us
+          </p>
+          <p
+            onClick={() => scrollToSection("contact-us")}
+            style={{ cursor: "pointer" }}
+          >
+            Contact Us
+          </p>
+          <p>Shipping</p>
         </div>
       </div>
       <div className="app-footer-div3">
         <div className="short-div-footer">
-            <div className="heading-footer-3">
-                <h5>CUSTOMER SUPPORT</h5>
+          <div className="heading-footer-3">
+            <h5>CUSTOMER SUPPORT</h5>
+          </div>
+          <div className="phone-footer">
+            <img src={vector} alt="img" />
+            <div className="phone-num">
+              <p>9489847336 , 9626731203</p>
             </div>
-            <div className="phone-footer">
-              <img src={vector} alt='img'/>
-              <div className="phone-num">
-                  <p>9489847336 ,
-                  9626731203</p>
-              </div>
+          </div>
+          <div className="mail-footer">
+            <img src={vector2} alt="img" />
+            <div className="phone-num">
+              <a
+                href="mailto:eintransport.service@gmail.com"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <p>eintransport.service@gmail.com</p>
+              </a>
             </div>
-            <div className="mail-footer">
-  <img src={vector2} alt='img'/>
-  <div className="phone-num">
-    <a href="mailto:eintransport.service@gmail.com" style={{ textDecoration: 'none', color: 'inherit' }}>
-      <p>eintransport.service@gmail.com</p>
-    </a>
-  </div>
-</div>
-<div className="para-res">
-  <p> Welcome to Eintransport! We are a trusted and reliable packing and moving service dedicated to making your relocation experience smooth and hassle-free.</p>
-</div>
-<div className="three-footer-res">
-<a href="https://www.instagram.com/eintransport?igsh=MWdnZ29sb3dhb2J5Yw==" target="_blank"> 
-  <img src={group} alt='img'/>
-</a>
+          </div>
+          <div className="para-res">
+            <p>
+              {" "}
+              Welcome to Eintransport! We are a trusted and reliable packing and
+              moving service dedicated to making your relocation experience
+              smooth and hassle-free.
+            </p>
+          </div>
+          <div className="three-footer-res">
+            <a
+              href="https://www.instagram.com/eintransport?igsh=MWdnZ29sb3dhb2J5Yw=="
+              target="_blank"
+            >
+              <img src={group} alt="img" />
+            </a>
 
-<a href="https://x.com/eintranspo92563?t=OKi5L5XsKMl_OAF2-s4Ftw&s=09" target="_blank"> 
-  <img src={group2} alt='img'/>
-</a>
-<a href="https://www.facebook.com/profile.php?id=61565638056769&mibextid=kFxxJD" target="_blank"> 
-  <img src={group3} alt='img'/>
-</a>
-
-
-        </div>
-        <div className="four-footers">
+            <a
+              href="https://x.com/eintranspo92563?t=OKi5L5XsKMl_OAF2-s4Ftw&s=09"
+              target="_blank"
+            >
+              <img src={group2} alt="img" />
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=61565764442291&mibextid=ZbWKwL"
+              target="_blank"
+            >
+              <img src={group3} alt="img" />
+            </a>
+          </div>
+          <div className="four-footers">
             <p>Copyright @2024</p>
-        </div>
+          </div>
         </div>
       </div>
     </div>
